a3f102a258051417131f41521cee1efa293bd0d7
Komissarts <komissarkomisan@gmail.com>
2022-06-12 11:12:19 +1000

    Update UIEvents.cs

--- Assets/Scripts/Event Systems/UIEvents.cs
+++ Assets/Scripts/Event Systems/UIEvents.cs
@@ -2,8 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public static class UIEvents
 {
+    private static void UpdateHappinessMeter()
+    {
 
+    }
+
+    private static void AddRecipe()
+    {
+
+    }
+
+    private static void RemoveRecipe()
+    {
+
+    }
+
+    private static void DisplayRecipe()
+    {
+
+    }
 }
