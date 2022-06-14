a4ea57a69c7ae16ccbf08a6661ab57e82ae7ced5
christa-zhang-uts <69833830+agltaria@users.noreply.github.com>
2022-06-14 18:49:33 +1000

    band animations + minor fixes
    Added band member animations and fixed crowd sprites a bit

--- Assets/Scripts/BPMMovement.cs
+++ Assets/Scripts/BPMMovement.cs
@@ -5,20 +5,20 @@ public class BPMMovement : MonoBehaviour
 {
     [SerializeField] float bpm; //possible placeholder - could make a static member somewhere else to get this value?
 
-    [Range(0, 1f)]
+    [Range(0, 0.2f)]
     [SerializeField] float yScale; //from 0-1.0
-    [Range(0, 1f)]
+    [Range(0, 0.2f)]
     [SerializeField] float xScale;
     float beat;
     [Range(10, 200)]
     //[SerializeField] int speed;
     [SerializeField] float test;
     static bool isStarted;
-
+    Vector3 originalScale;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalScale = transform.localScale;
 
     }
 
@@ -29,19 +29,19 @@ public class BPMMovement : MonoBehaviour
         beat = bpm / 60; //bps
         //test = -Mathf.Cos(360 * Time.deltaTime);
         //StartCoroutine("Bopping");
-        float x = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + (1 + (xScale / 2));
-        test = x;
-        float y = -(yScale/2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + ( 1 + (yScale/2));
+        float x = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + (originalScale.x + (xScale / 2));
+        
+        float y = -(yScale/2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + ( originalScale.y + (yScale/2));
         
         transform.localScale = new Vector3(x, y, 1);
     }
 
     IEnumerator Bopping()
     {
-        
-        float x = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * bpm / 60) + (1 + (xScale / 2));
-        test = x;
-        float y = -(yScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * bpm / 60) + (1 + (yScale / 2));
+
+        float x = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + (originalScale.x + (xScale / 2));
+
+        float y = -(yScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + (originalScale.y + (yScale / 2));
 
         transform.localScale = new Vector3(x, y, 1);
         yield return new WaitForSeconds(beat);
--- Assets/Scripts/CrowdMovement.cs
+++ Assets/Scripts/CrowdMovement.cs
@@ -6,36 +6,40 @@ public class CrowdMovement : MonoBehaviour
 {
     [SerializeField] float bpm; //possible placeholder - could make a static member somewhere else to get this value?
 
-    [Range(0, 1f)]
+    [Range(0, 0.2f)]
     [SerializeField] float yTransform; //boing
-    [Range(0, 1f)]
+    [Range(0, 0.2f)]
     [SerializeField] float xScale;
     float beat;
 
     [SerializeField] float test;
    
     static bool isStarted;
+    [SerializeField] bool isBopping;
+    [SerializeField] bool isVibing;
     [SerializeField] bool isReversed;
 
     Vector3 originalPos;
     // Start is called before the first frame update
     void Start()
     {
-        originalPos = transform.position;
-
+        originalPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       
         beat = bpm / 60; //bps
-        float scaleX = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat) + (1 + (xScale / 2));
-        float transformY = -yTransform * Mathf.Cos((Mathf.PI * 2 * Time.time) * beat);
+        float scaleX = (xScale / 2) * Mathf.Cos((Mathf.PI * 2 * (float)AudioSettings.dspTime) * beat) + (1 + (xScale / 2));
+        float transformY = -yTransform * Mathf.Cos((Mathf.PI * 2 * (float)AudioSettings.dspTime) * beat);
         if (isReversed)
             transformY = -transformY;
+
+        if (isBopping)
+            transform.localPosition = new Vector3(originalPos.x, originalPos.y + transformY, originalPos.z);
         
-        transform.localPosition = new Vector3(originalPos.x, originalPos.y + transformY, originalPos.z);
-        transform.localScale = new Vector3(scaleX, 1, 1);
+        if(isVibing)
+            transform.localScale = new Vector3(scaleX, 1, 1);
     }
 }
