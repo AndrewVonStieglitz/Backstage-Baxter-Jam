15b171116e9f944555e1a121ce375b66985e9a15
JohnCollns <50039786+JohnCollns@users.noreply.github.com>
2022-06-30 21:14:34 +1000

    Prepared Level 1 for Testing and Fixing
    Now uses the correct plugCables rather than antiquated amp/speakers.
    Problem: Game crashes when you connect a cable

--- Assets/Scripts/Recipe System/RecipeData.cs
+++ Assets/Scripts/Recipe System/RecipeData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "RecipeSystemScriptableObjects/New Recipe")]
 public class RecipeData : ScriptableObject
 {
     public recipe Recipe
